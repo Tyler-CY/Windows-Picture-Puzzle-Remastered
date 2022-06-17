85d5872512568556dfff939027c9637a37f03520
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-06-16 21:52:52 -0400

    Added more documentations in code

--- popup.js
+++ popup.js
@@ -1,23 +1,27 @@
-
-
 addListenerToGridItems()
 document.getElementById("hint").addEventListener("click", handleHintButton);
 
+
+/*
+Helper Functions
+ */
 
 function handleHintButton(){
     const gridList = document.querySelectorAll(".grid-item");
     const length = gridList.length;
 
     // gridList[0].style.display is originally "" (empty string)
+    // Determine what the visibility of the 16 grids should be changed to.
     var visibility = "block";
     if (gridList[0].style.display != "none"){
         visibility = "none";
     }
-
     for (let i = 0; i < length; i++) {
+        // Change the visibility of the 16 grids.
         gridList[i].style.display = visibility;
     }
 
+    // Change the background to the final full image as a hint, and revert it back if the hint button is clicked again.
     if (visibility == "none"){
         document.getElementById("innerWindow").style.backgroundImage="url('res/numbers/numbers_full.png')";
     }
@@ -27,10 +31,13 @@ function handleHintButton(){
 }
 
 function addListenerToGridItems() {
+    // Find all grid-items
     const gridList = document.querySelectorAll(".grid-item");
-    const length = gridList.length;
-    for (let i = 0; i < length; i++) {
 
+    const length = gridList.length;
+
+    // For each grid-item (in gridList), prevent the grid image from being dragged and add moving function to each grid.
+    for (let i = 0; i < length; i++) {
         if (gridList[i].firstChild){
             gridList[i].firstChild.setAttribute("draggable", false);
         }
@@ -42,16 +49,22 @@ function deleteFirstChild(){
     var parentId = event.currentTarget.id
     var parentElement = document.getElementById(parentId);
 
+    // Extract the grid number (e.g. extract 15 from "grid15").
     var gridNumber = Number(parentId.toString().slice(4));
 
+    // ifCondition checks if any of the four neighbour grids is empty.
     var ifCondition = false;
+    // targetGrid is the grid which is currently empty on the board.
     var targetGrid;
 
+    // Update ifCondition and targetGrid by checking the four adjacent grids.
     const topGridItem = document.getElementById("grid" + (gridNumber - 4));
+    // If the adjacent grid is empty, then it has no childNodes.
     if (topGridItem != null && topGridItem.childNodes.length == 0){
         ifCondition = true;
         targetGrid = topGridItem;
     }
+    // Logic same as above.
     const bottomGridItem = document.getElementById("grid" + (gridNumber + 4));
     if (bottomGridItem != null && bottomGridItem.childNodes.length == 0){
         ifCondition = true;
@@ -68,6 +81,7 @@ function deleteFirstChild(){
         targetGrid = rightGridItem;
     }
 
+    // Check if we can move the tile.
     if (ifCondition){
             document.getElementById("testingOutput").innerText = gridNumber;
             targetGrid.appendChild(parentElement.firstChild);
