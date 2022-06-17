5ddf38a456b9b2330154bab5b88fee962f998879
TylerCYan <90355620+TylerCYan@users.noreply.github.com>
2022-06-17 02:06:17 -0400

    Fixed GridRol/Col bug + Refactored Code

--- popup.js
+++ popup.js
@@ -7,41 +7,38 @@ Helper Functions
  */
 
 // Shuffle the tiles
-function handleSwapButton(){
-    for (let i = 0; i < 100; i++){
+function handleSwapButton() {
+    for (let i = 0; i < 500; i++) {
         makeRandomMove();
     }
 
-    var emptyGrid = findEmptyGrid();
-    // Extract the grid number (e.g. extract 15 from "grid15").
-    const gridNumber = Number(emptyGrid.id.toString().slice(4));
+    const emptyGrid = findEmptyGrid();
     // Extract grid row and column
-    let gridRow = Number(String(gridNumber).slice(0, 1));
-    let gridCol = Number(String(gridNumber).slice(1));
+    let gridRow = Number(emptyGrid.id.slice(4, 5));
+    let gridCol = Number(emptyGrid.id.slice(5));
+    console.log(gridRow, gridCol);
 
     // Moves the empty slot to the bottom right corner.
-    while (gridRow !== 3){
+    while (gridRow !== 3) {
         moveTileById("grid" + (gridRow + 1) + gridCol);
         gridRow += 1;
     }
-    while (gridCol !== 3){
+    while (gridCol !== 3) {
         moveTileById("grid" + gridRow + (gridCol + 1));
         gridCol += 1;
     }
 }
 
 // Makes a random move based on the position of the empty grid
-function makeRandomMove(){
-    var emptyGrid = findEmptyGrid();
+function makeRandomMove() {
+    const emptyGrid = findEmptyGrid();
 
     // Find out the number of adjacent grids to the empty grid so we can randomly draw on of the grids to move
-    var adjacentGrids = [];
+    let adjacentGrids = [];
 
-    // Extract the grid number (e.g. extract 15 from "grid15").
-    const gridNumber = Number(emptyGrid.id.toString().slice(4));
     // Extract grid row and column
-    const gridRow = Number(String(gridNumber).slice(0, 1));
-    const gridCol = Number(String(gridNumber).slice(1));
+    let gridRow = Number(emptyGrid.id.slice(4, 5));
+    let gridCol = Number(emptyGrid.id.slice(5));
 
     // Update numOfAdjacentGrids by checking the four adjacent grids.
     const topGridItem = document.getElementById("grid" + (gridRow - 1) + gridCol);
@@ -50,15 +47,15 @@ function makeRandomMove(){
         adjacentGrids.push(topGridItem);
     }
     // Logic same as above.
-    const bottomGridItem = document.getElementById("grid" + + (gridRow + 1) + gridCol);
+    const bottomGridItem = document.getElementById("grid" + +(gridRow + 1) + gridCol);
     if (bottomGridItem) {
         adjacentGrids.push(bottomGridItem);
     }
-    const leftGridItem = document.getElementById("grid" + gridRow + (gridCol + 1));
+    const leftGridItem = document.getElementById("grid" + gridRow + (gridCol - 1));
     if (leftGridItem) {
         adjacentGrids.push(leftGridItem);
     }
-    const rightGridItem = document.getElementById("grid" + gridRow + (gridCol - 1));
+    const rightGridItem = document.getElementById("grid" + gridRow + (gridCol + 1));
     if (rightGridItem) {
         adjacentGrids.push(rightGridItem);
     }
@@ -69,7 +66,7 @@ function makeRandomMove(){
 }
 
 // Returns the grid-item element (div) which is currently empty.
-function findEmptyGrid(){
+function findEmptyGrid() {
     // Find all grid-items
     const gridList = document.querySelectorAll(".grid-item");
 
@@ -122,20 +119,15 @@ function addListenerToGridItems() {
                 return false;
             };
         }
-        gridList[i].addEventListener("click", moveTile);
+        gridList[i].addEventListener("click", moveTileByButton);
     }
 }
 
-// Moves the tile on the current grid to the adjacent available grid.
-function moveTile() {
-    const parentId = event.currentTarget.id;
-    const parentElement = document.getElementById(parentId);
-
-    // Extract the grid number (e.g. extract 15 from "grid15").
-    const gridNumber = Number(parentId.toString().slice(4));
+function moveSingleTile(parentId) {
     // Extract grid row and column
-    const gridRow = Number(String(gridNumber).slice(0, 1));
-    const gridCol = Number(String(gridNumber).slice(1));
+    const gridRow = Number(parentId.slice(4, 5));
+    const gridCol = Number(parentId.slice(5));
+
 
     // ifCondition checks if any of the four neighbour grids is empty.
     let ifCondition = false;
@@ -150,17 +142,17 @@ function moveTile() {
         targetGrid = topGridItem;
     }
     // Logic same as above.
-    const bottomGridItem = document.getElementById("grid" + + (gridRow + 1) + gridCol);
+    const bottomGridItem = document.getElementById("grid" + (gridRow + 1) + gridCol);
     if (bottomGridItem != null && bottomGridItem.childNodes.length === 0) {
         ifCondition = true;
         targetGrid = bottomGridItem;
     }
-    const leftGridItem = document.getElementById("grid" + gridRow + (gridCol + 1));
+    const leftGridItem = document.getElementById("grid" + gridRow + (gridCol - 1));
     if (leftGridItem != null && leftGridItem.childNodes.length === 0) {
         ifCondition = true;
         targetGrid = leftGridItem;
     }
-    const rightGridItem = document.getElementById("grid" + gridRow + (gridCol - 1));
+    const rightGridItem = document.getElementById("grid" + gridRow + (gridCol + 1));
     if (rightGridItem != null && rightGridItem.childNodes.length === 0) {
         ifCondition = true;
         targetGrid = rightGridItem;
@@ -168,52 +160,18 @@ function moveTile() {
 
     // Check if we can move the tile.
     if (ifCondition) {
+        const parentElement = document.getElementById(parentId);
         targetGrid.appendChild(parentElement.firstChild);
     }
 }
 
 // Moves the tile on the current grid to the adjacent available grid.
+function moveTileByButton() {
+    const parentId = event.currentTarget.id;
+    moveSingleTile(parentId);
+}
+
+// Moves the tile on the current grid to the adjacent available grid.
 function moveTileById(parentId) {
-    const parentElement = document.getElementById(parentId);
-
-    // Extract the grid number (e.g. extract 15 from "grid15").
-    const gridNumber = Number(parentId.toString().slice(4));
-    // Extract grid row and column
-    const gridRow = Number(String(gridNumber).slice(0, 1));
-    const gridCol = Number(String(gridNumber).slice(1));
-
-    // ifCondition checks if any of the four neighbour grids is empty.
-    let ifCondition = false;
-    // targetGrid is the grid which is currently empty on the board.
-    let targetGrid;
-
-// Update ifCondition and targetGrid by checking the four adjacent grids.
-    const topGridItem = document.getElementById("grid" + (gridRow - 1) + gridCol);
-    // If the adjacent grid is empty, then it has no childNodes.
-    if (topGridItem != null && topGridItem.childNodes.length === 0) {
-        ifCondition = true;
-        targetGrid = topGridItem;
-    }
-    // Logic same as above.
-    const bottomGridItem = document.getElementById("grid" + + (gridRow + 1) + gridCol);
-    if (bottomGridItem != null && bottomGridItem.childNodes.length === 0) {
-        ifCondition = true;
-        targetGrid = bottomGridItem;
-    }
-    const leftGridItem = document.getElementById("grid" + gridRow + (gridCol + 1));
-    if (leftGridItem != null && leftGridItem.childNodes.length === 0) {
-        ifCondition = true;
-        targetGrid = leftGridItem;
-    }
-    const rightGridItem = document.getElementById("grid" + gridRow + (gridCol - 1));
-    if (rightGridItem != null && rightGridItem.childNodes.length === 0) {
-        ifCondition = true;
-        targetGrid = rightGridItem;
-    }
-
-
-    // Check if we can move the tile.
-    if (ifCondition) {
-        targetGrid.appendChild(parentElement.firstChild);
-    }
+    moveSingleTile(parentId);
 }
